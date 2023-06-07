1ff3c12ba577e8032fc1fca8200d7b158f7c1b9c
Sriram Yagnaraman <sriram.yagnaraman@est.tech>
2023-06-07 13:25:00 +0200

    add usrsctp submodule and UDP reflector application

---
+++ xdp-udp-reflector/xdp_udp_reflector_kern.c
@@ -0,0 +1,96 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <netinet/in.h>
+#include <bpf/bpf_endian.h>
+
+#if DEBUG
+#define Dx(fmt, ...)                                      \
+    ({                                                         \
+        char ____fmt[] = fmt;                                  \
+        bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
+    })
+#else
+#define Dx(fmt, ...)
+#endif
+
+// Socket map for redirect to user-space
+struct {
+	__uint(type, BPF_MAP_TYPE_XSKMAP);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
+	__uint(max_entries, 1); /* Must be > nqueues for the nic */
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+} xdp_udp_xsks SEC(".maps");
+
+static __always_inline int handle_ipv4(struct xdp_md *xdp)
+{
+	void *data_end = (void *)(long)xdp->data_end;
+	void *data = (void *)(long)xdp->data;
+	struct iphdr *iph = data + sizeof(struct ethhdr);
+
+	if (iph + 1 > data_end)
+		return XDP_DROP;
+
+	switch (iph->protocol) {
+	case IPPROTO_UDP:
+		break;
+	default:
+		return XDP_PASS;
+	}
+
+	int rc = bpf_redirect_map(&xdp_udp_xsks, xdp->rx_queue_index, XDP_PASS);
+	Dx("UDP XDP redirect (v4), len=%ld, rc=%d\n", (data_end - data), rc);
+	return rc;
+}
+
+static __always_inline int handle_ipv6(struct xdp_md *xdp)
+{
+	void *data_end = (void *)(long)xdp->data_end;
+	void *data = (void *)(long)xdp->data;
+	struct ipv6hdr *ip6h = data + sizeof(struct ethhdr);
+
+	if (ip6h + 1 > data_end)
+		return XDP_DROP;
+
+	switch (ip6h->nexthdr) {
+	case IPPROTO_UDP:
+		break;
+	default:
+		return XDP_PASS;
+	}
+
+	int rc = bpf_redirect_map(&xdp_udp_xsks, xdp->rx_queue_index, XDP_PASS);
+	Dx("UDP XDP redirect (v6), len=%ld, rc=%d\n", (data_end - data), rc);
+	return rc;
+}
+
+SEC("xdp.frags")
+int _xdp_udp_redirect(struct xdp_md *xdp)
+{
+	void *data_end = (void *)(long)xdp->data_end;
+	void *data = (void *)(long)xdp->data;
+	struct ethhdr *eth = data;
+
+	if (eth + 1 > data_end)
+		return XDP_DROP;
+
+	int rc = XDP_PASS;
+	switch(bpf_ntohs(eth->h_proto)) {
+	case ETH_P_IP:
+		rc = handle_ipv4(xdp);
+		break;
+	case ETH_P_IPV6:
+		rc = handle_ipv6(xdp);
+		break;
+	default:
+		break;
+	}
+
+	return rc;
+}
+
+char _license[] SEC("license") = "GPL";
---
+++ xdp-udp-reflector/xdp_udp_reflector_user.c
@@ -0,0 +1,193 @@
+/*
+   SPDX-License-Identifier: MIT
+   Copyright (c) Nordix Foundation
+*/
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <poll.h>
+#include <unistd.h>
+#include <xdp/libxdp.h>
+#include <xdp/xsk.h>
+#include <net/if.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <arpa/inet.h>
+
+#include <xdp/libxdp.h>
+#include <xdp/xsk.h>
+
+#include "common/log.h"
+#include "common/mempool.h"
+#include "common/port.h"
+#include "common/prog.h"
+
+static void udp_reflector(void *data)
+{
+	struct ethhdr *eth = (struct ethhdr *)data;
+	struct iphdr *ip = (struct iphdr *)((void*)eth + sizeof(eth));
+	struct udphdr *udp = (struct udphdr *)((void*)ip + sizeof(ip));
+
+	unsigned char tmp_mac[ETH_ALEN];
+	uint32_t tmp_ip;
+	uint16_t tmp_port;
+
+	/* SWAP MAC address */
+	memcpy(&tmp_mac[0], &eth->h_source[0], sizeof(tmp_mac));
+	memcpy(&eth->h_source[0], &eth->h_dest[0], sizeof(tmp_mac));
+	memcpy(&eth->h_dest[0], &tmp_mac[0], sizeof(tmp_mac));
+
+	/* SWAP IP address */
+	tmp_ip = ip->saddr;
+	ip->saddr = ip->daddr;
+	ip->daddr = tmp_ip;
+	ip->ttl = 0xFF;
+
+	/* SWAP UDP port */
+	tmp_port = udp->source;
+	udp->source = udp->dest;
+	udp->dest = tmp_port;
+}
+
+static int start_rx(char const *dev, char const *prog, int q, unsigned nfq)
+{
+	unsigned int ifindex = if_nametoindex(dev);
+	if (ifindex == 0)
+		die("Unknown interface [%s]\n", dev);
+
+	struct xdp_program *xdp_prog = xdp_prog_load(
+		prog, ifindex, XDP_MODE_NATIVE);
+	if (!xdp_prog)
+		die("ERROR: xdp program not loaded: %s\n", strerror(errno));
+
+	struct mempool *mp = mempool_create(&mempool_params_default, &umem_config_default);
+	if (!mp)
+		die("ERROR: Memory pool NOT created: %s\n", strerror(errno));
+
+	struct port_params pparms = {
+		.xsk_config.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS,
+		.xsk_config.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS,
+		.xsk_config.libxdp_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD,
+		.xsk_config.bind_flags = XDP_USE_NEED_WAKEUP,
+		.mp = mp,
+		.queue = q
+	};
+	pparms.ifname = strdup(dev);
+
+	struct port *p = port_create(&pparms);
+	if (!p)
+		die("ERROR: Port NOT created: %s\n", strerror(errno));
+
+	xdp_map_update(xdp_prog, "xdp_udp_xsks", p->xsk);
+
+	struct pkt_burst rx_burst;
+	struct pkt_burst tx_burst;
+	uint32_t nb_pkts = mp->fill_size;
+	memset(&rx_burst, 0, sizeof(rx_burst));
+	memset(&tx_burst, 0, sizeof(tx_burst));
+
+	for (;;) {
+		if (nb_pkts) {
+			port_fq_setup(p, nb_pkts);
+			port_cq_setup(p, nb_pkts);
+			memset(&rx_burst, 0, sizeof(rx_burst));
+			memset(&tx_burst, 0, sizeof(tx_burst));
+		}
+
+		port_rx_burst(p, &rx_burst);
+		nb_pkts = rx_burst.nb_pkts;
+
+		if (!nb_pkts) {
+			usleep(10 * 1000);
+			continue;
+		}
+
+		for (int i = 0; i < nb_pkts; i++) {
+			uint64_t addr = rx_burst.addr[i];
+			uint32_t len = rx_burst.len[i];
+
+			if (len < ETH_HLEN) {
+				printf("Short frame %d\n", len);
+				continue;
+			}
+
+			uint8_t *pkt = xsk_umem__get_data(mp->addr, addr);
+			udp_reflector(pkt);
+
+			tx_burst.addr[tx_burst.nb_pkts] = addr;
+			tx_burst.len[tx_burst.nb_pkts] = len;
+			tx_burst.nb_pkts++;
+		}
+
+		port_tx_burst(p, &tx_burst);
+	}
+
+	return EXIT_SUCCESS;
+}
+
+static struct option long_options[] = {
+	{"help", no_argument, NULL, 'h'},
+	{"dev", required_argument, NULL, 'd'},
+	{"queue", required_argument, NULL, 'q'},
+	{"fillq", required_argument, NULL, 'f'},
+	{"prog", required_argument, NULL, 'p'},
+	{NULL, 0, NULL, 0}
+};
+
+void print_usage()
+{
+	printf("Usage: \n");
+	for(int i = 0; i < sizeof(long_options)/sizeof(struct option) - 1; i++) {
+		printf("--%s (%c) arg: %s\n",
+			long_options[i].name, long_options[i].val,
+			long_options[i].has_arg ? "<required>" : "");
+	}
+	printf("\n");
+}
+
+int main(int argc, char **argv)
+{
+	char ch;
+	char const *dev = NULL;
+	char const *prog = NULL;
+	int queue = 0;
+	int fillq = 512;
+
+	while ((ch = getopt_long(argc, argv, "hd:q:f:p:", long_options, NULL)) != -1) {
+		switch (ch)
+		{
+		case 'h':
+			print_usage();
+			return 0;
+		case 'd':
+			dev = strdup(optarg);
+			break;
+		case 'q':
+			queue = atoi(optarg);
+			break;
+		case 'f':
+			fillq = atoi(optarg);
+			break;
+		case 'p':
+			prog = strdup(optarg);
+		default:
+			break;
+		}
+	}
+
+	if (dev == NULL) {
+		print_usage();
+		die("--dev is a required argument\n");
+	} else if (prog == NULL) {
+		prog = strdup("/usr/local/lib/bpf/xdp_sctp_kern.o");
+		printf("No program provided, using default BPF prog: %s!!!\n", prog);
+	}
+
+	argc -= optind;
+	argv += optind;
+	return start_rx(dev, prog, queue, fillq);
+}
