a53e0c9e1e90b9da3c010e49080502d555001706
Sriram Yagnaraman <sriram.yagnaraman@est.tech>
2023-06-22 11:34:24 +0200

    test/ovl/ebpf: add support qemu igb emulated device

--- test/ovl/ebpf/net-setup.sh
+++ test/ovl/ebpf/net-setup.sh
@@ -1,67 +1,41 @@
 #! /bin/sh
 
-die() {
-	echo "ERROR: $*" >&2
-	exit 1
-}
+nodeid=$1
+n=$2
 
-# "standard" xcluster setup;
-netX() {
-	local nodeid=$1
-	local n=$2
-	local tap b0 b1 dev
+dev=xcbr$n
+ip link show dev $dev > /dev/null 2>&1 || \
+	die "Bridge does not exists [$dev]"
+tap=${dev}_t$nodeid
+b1=$n
 
-	dev=xcbr$n
-	ip link show dev $dev > /dev/null 2>&1 || \
-		die "Bridge does not exists [$dev]"
-	tap=${dev}_t$nodeid
-	b1=$n
+dut=igb
+num_queues=4
+mq=""
 
-	test "$__mtu" -ne 1500 && append="$append mtu$n=$__mtu"
-
-	if ip link show dev $tap > /dev/null 2>&1; then
-		echo "Tap device already exist [$tap]"
-	else
-		ip tuntap add $tap mode tap user $USER
-		ip link set mtu $__mtu dev $tap
-		ip link set dev $tap master $dev
-		ip link set up $tap
+if test $n -ge 1; then
+	if test $num_queues -gt 1; then
+		mq="multi_queue"
 	fi
-
-	b0=$(printf '%02x' $nodeid)
-	echo " -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap"
-	echo " -device virtio-net-pci,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
-}
-
-# Mqueue setup. Needed for XDP with 0-copy
-netY() {
-	local nodeid=$1
-	local n=$2
-	local tap b0 b1 dev tmp
-
-	dev=xcbr$n
-	tap=${dev}_t$nodeid
-
-	tmp=/tmp/tmp/xcnet
-	mkdir -p $tmp
-	cat > $tmp/$tap <<EOF
-#! /bin/sh
-echo $tap \$1 > $tmp/$tap.log
-ip tuntap add \$1 mode tap user $USER
-ip link set dev \$1 master $dev
-ip link set up \$1
-EOF
-	chmod a+x $tmp/$tap
-	
-	b0=$(printf '%02x' $nodeid)
-	b1=$n
-	echo " -netdev tap,ifname=$tap,id=net$n,script=$tmp/$tap,queues=4,vhost=on"
-	echo " -device virtio-net-pci,mq=on,vectors=6,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
-}
-
-if test "$2" != "0"; then
-	netY $1 $2
-else
-	netX $1 $2
 fi
 
+if ip link show dev $tap > /dev/null 2>&1; then
+	ip link del $tap > /dev/null 2>&1
+fi
+
+ip tuntap add $tap mode tap user $USER $mq
+ip link set mtu $__mtu dev $tap
+ip link set dev $tap master $dev
+ip link set up $tap
+
+
+b0=$(printf '%02x' $nodeid)
+if test $n -ge 1; then
+	echo "$opt -device pcie-root-port,slot=$n,id=pcie_port.$n"
+	echo "$opt -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap,queues=$num_queues"
+	echo "$opt -device $dut,bus=pcie_port.$n,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
+else
+	echo "$opt -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap"
+	echo "$opt -device virtio-net-pci,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
+	#echo "$opt -device e1000,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
+fi
